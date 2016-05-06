714d25bdd6e90331f36829c493ec95f7a9926c80
Tim Evans <timevans@asu.edu>
2016-05-05 18:58:54 -0700

    Flipit function added

--- shadow.sh
+++ shadow.sh
@@ -4,13 +4,34 @@
 # set this to the location of ImageMagick's convert command
 CONVERTER="/usr/local/bin/convert"
 # set this to the location where your Image geeklet will look
-OUTFILE="$HOME/reflected.png"
+OUTDIRECTORY="$HOME"
 TMPDIR=~/tmp
 # ===========================================================
 
 # don't mess with these
 workdir=$TMPDIR
-infile="$workdir/combo.png"
+
+# start functions
+flipit(){
+	infile=$1
+	outfile=$2
+	# set the width and height variables
+	ww=`$CONVERTER $infile -format "%w" info:`
+	hh=`$CONVERTER $infile -format "%h" info:`
+	hhr=`$CONVERTER xc: -format "%[fx:$hh*40/50]" info:`
+	# clone the original image, flip it, composite a gradient and append it below the original
+	$CONVERTER $infile \
+	\( -size ${ww}x0 xc:none \) \
+	\( -clone 0 -flip +repage \) \
+	\( -clone 0 -alpha extract -flip +repage \
+	-size ${ww}x${hh} gradient: +level 0x50% \
+	-compose multiply -composite \) \
+	\( -clone 2 -clone 3 -alpha off -compose copy_opacity -composite \) \
+	-delete 2,3 -channel rgba -alpha on -background none -append $outfile
+}
+
+
+
 
 mkdir -p $TMPDIR
 
@@ -20,17 +41,9 @@ date +'%A %I:%M%p' | $CONVERTER -background none -fill white -font Helvetica -po
 date +'%B %d'      | $CONVERTER -background none -fill white -font Helvetica -pointsize 200 -trim label:@- $workdir/month.png
 
 
-# set the width and height variables
-ww=`$CONVERTER $infile -format "%w" info:`
-hh=`$CONVERTER $infile -format "%h" info:`
-hhr=`$CONVERTER xc: -format "%[fx:$hh*40/50]" info:`
-# clone the original image, flip it, composite a gradient and append it below the original
-$CONVERTER $infile \
-\( -size ${ww}x0 xc:none \) \
-\( -clone 0 -flip +repage \) \
-\( -clone 0 -alpha extract -flip +repage \
--size ${ww}x${hh} gradient: +level 0x50% \
--compose multiply -composite \) \
-\( -clone 2 -clone 3 -alpha off -compose copy_opacity -composite \) \
--delete 2,3 -channel rgba -alpha on -background none -append $OUTFILE
+flipit "$workdir/year.png" "$OUTDIRECTORY/year.png"
+flipit "$workdir/day.png" "$OUTDIRECTORY/day.png"
+flipit "$workdir/month.png" "$OUTDIRECTORY/month.png"
+
+
 exit 0
